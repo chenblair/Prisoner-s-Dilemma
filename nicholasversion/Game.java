2d3dec808ebcfdd5994b70b7958042b24f02fdff
Merrycatch22 <tarn.nicholas@gmail.com>
2015-06-26 20:52:59 -0700

    added documentation for nicholasversion

--- nicholasversion/Game.java
+++ nicholasversion/Game.java
@@ -3,6 +3,9 @@ package nicholasversion;
 import java.util.ArrayList;
 
 public class Game {
+	/// pal holds the two players in the game.
+	/// the final integers is the score awarded for the different possibilities.
+	/// the playerTypes are passed to the Players as their types.
 	public ArrayList<Player> pal;
 	public final int BOTHCOOP=2,LOSE=0,WIN=3,BOTHDEFECT=1;
 	public int playerType1,playerType2;
@@ -17,7 +20,9 @@ public class Game {
 		pal.add(new Player(playerType1));
 		pal.add(new Player(playerType2));
 	}
-
+	/**
+	 * loops the two competitors over multiple games, then prints their score at the end.
+	 */
 	public void loopAndPrint() {
 		System.out.printf("Game between %s and %s:\n",pal.get(0).getTYPENAME(),pal.get(1).getTYPENAME());
 		for (int i = 0; i < 100; i++) {
@@ -26,7 +31,13 @@ public class Game {
 		pal.get(0).printScore();
 		pal.get(1).printScore();
 	}
-
+	/**
+	 * Players play one game and record results and update info.
+	 * Based on how they are played, addScore will add the different values.
+	 * the record of what they have played is also recorded.
+	 * @param p1
+	 * @param p2
+	 */
 	public void interact(Player p1, Player p2) {
 		
 		boolean p1Strat=p1.play();
@@ -61,7 +72,10 @@ public class Game {
 		p2.incrementRound();
 
 	}
-
+	/**
+	 * main is here until I move it elsewhere.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		int amtOfPlayerTypes=4;
 		ArrayList<Game> gameAL=new ArrayList<Game>();
--- nicholasversion/Player.java
+++ nicholasversion/Player.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 
 
 public class Player {
-	
+	///playerType stores what type of player it is.
+	///the score is how much score they have gotten in their games total.
+	///the round stores how many rounds they have played.
+	///recordKeeper stores the game values.
+	///final ints are for finding which index has what value in record keeping.
+	///TYPENAMES stores what name is associated with each playerType.
 	private int playerType;
 	private int score;
 	private int round;
@@ -18,12 +23,23 @@ public class Player {
 		this.playerType=playerType;
 		score=0;
 		recordKeeper=new ArrayList<boolean[]>();
+		round=0;
 	}
 
 	public String getTYPENAME(){
 		return TYPENAMES[playerType];
 	}
 	
+	/**
+	 * depending on what playerType the Player is, the strategy returned is different.
+	 * true is cooperate
+	 * false is defect.
+	 * 0 Angel will always return true
+	 * 1 Devil will always return false
+	 * 2 TTCoop will return true the first game, then play what his opponent did the previous.
+	 * 3 TTDefect will return false the first game, then play what his opponent did the previous.
+	 * @return
+	 */
 	public boolean play(){
 		boolean retBool=false;
 		
@@ -59,13 +75,18 @@ public class Player {
 		
 		return retBool;
 	}
-	
+	/**
+	 * adds a record of game to the recordKeeper.
+	 * @param record
+	 */
 	public void addRecord(boolean[] record){
 		recordKeeper.add(record);
 	}
+
 	public void addScore(int addition){
 		score+=addition;
 	}
+	
 	public void incrementRound(){
 		round++;
 	}
